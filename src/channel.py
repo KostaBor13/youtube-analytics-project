49a1bab671afa00da11003157d550c97d5c70924
Kostas <zaikin399@mail.ru>
2024-02-04 14:15:52 +0300

    написание методов

--- src/channel.py
+++ src/channel.py
@@ -24,6 +24,7 @@ class Channel:
         self.video_count = self.channel["items"][0]["statistics"]["videoCount"]
         self.view_count = self.channel["items"][0]["statistics"]["viewCount"]
 
+
     @classmethod
     def get_service(cls):
         return cls.youtube
@@ -32,6 +33,29 @@ class Channel:
     def channel_id(self) -> str:
         return self.__channel_id
 
+    def __str__(self):
+        return f'{str(self.title)} ({self.url})'
+
+    def __add__(self, other):
+        return int(self.subscriber_count) + int(other.subscriber_count)
+
+    def __sub__(self, other):
+        return int(self.subscriber_count) - int(other.subscriber_count)
+
+    def __gt__(self, other):
+        return int(self.subscriber_count) > int(other.subscriber_count)
+
+    def __ge__(self, other):
+        return int(self.subscriber_count) >= int(other.subscriber_count)
+
+    def __lt__(self, other):
+        return int(self.subscriber_count) < int(other.subscriber_count)
+
+    def __le__(self, other):
+        return int(self.subscriber_count) <= int(other.subscriber_count)
+
+    def __eq__(self, other):
+        return int(self.subscriber_count) == int(other.subscriber_count)
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
